3f3de0cffc67ec87b2239c9d6682c1b20d0b2d4a
Jan-Albert03 <jaschg1900@alumno.ipn.mx>
2021-10-05 23:08:40 -0500

    des

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gaemil
+ */
+public class Main {
+    public static void main(String args[]){
+        Cifrar x = new Cifrar();
+        x.setVisible(true);
+        Descifrar y = new Descifrar();
+        y.setVisible(true);
+    }
+}
